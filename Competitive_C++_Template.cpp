584fe1f33ade75ddec3de352512825c051691ac0
SteveDengZishi <zd475@nyu.edu>
2017-02-17 04:54:45 -0500

    adding template

---
+++ Competitive_C++_Template.cpp
@@ -0,0 +1,41 @@
+//  Created by DENG ZISHI on 2/17/17.
+//  Copyright © 2017 DENG ZISHI. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+//macro here
+#define FOR(i,a,b) for(size_t i=a;i<b;i++)
+#define DE(x) cout << #x << ":" << x << endl
+
+//name space here
+using namespace std;
+
+//typedef here
+typedef vector<int> VI;
+
+//debugging functions here
+void DEVEC(VI vec){
+    FOR(i,0,vec.size()){
+        DE(vec[i]);
+    }
+}
+
+//Non-STL Data Structures definition here
+
+
+//start of main()
+int main(int argc, const char * argv[]) {
+    //optimize iostream
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    //Your code here
+    
+    
+    return 0;
+}
