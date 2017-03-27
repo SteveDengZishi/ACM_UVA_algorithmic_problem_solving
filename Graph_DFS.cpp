75d44ef8c21722d6b81ba34a7402317a47db1149
SteveDengZishi <zd475@nyu.edu>
2017-03-26 22:59:30 -0400

    Graphs_dfs

--- Graph_DFS.cpp
+++ Graph_DFS.cpp
@@ -1,45 +1,74 @@
-//  Graph with DFS
-//  Only partially done
-
+//  C++ program to print DFS traversal from a given vertex in a  given graph
 //  Created by DENG ZISHI on 2/20/17.
 //  Copyright © 2017 DENG ZISHI. All rights reserved.
 //
-#include <unordered_map>
+
 #include <list>
-#include <stack>
 
-template <class T>
-struct Node{
-    bool visited;
-    T data;
-    
-    typedef unordered_map<Node,list<Node>> adjacencyList;
-    
-    Node(T info):visited(0),data(info){}
+using namespace std;
+
+// Graph class represents a directed graph using adjacency list representation
+class Graph
+{
+    int V;    // No. of vertices
+    list<int> *adj;    // Pointer to an array containing adjacency lists
+    void DFSUtil(int v, bool visited[]);  // A function used by DFS
+public:
+    Graph(int V);   // Constructor
+    void addEdge(int v, int w);   // function to add an edge to graph
+    void DFS(int v);    // DFS traversal of the vertices reachable from v
 };
 
-template <class T>
-struct Graph{
-    int size;
-    typename Node<T>::adjacencyList adjL;
-    
-    bool isConnected(T x,T y){
-        return false;
-    }
-    
-    void addEdge(T x,T y){
-        
-    }
-    
-    void dfs(T start,T end){
-        stack<T> st; //build a stack to begin dfs
-        st.push(start);
-        Node<T> begin(start);
-        //mark visited
-        
-    }
-    
-    int solve(){
-        
-    }
-};
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w); // Add w to v’s list.
+}
+
+void Graph::DFSUtil(int v, bool visited[])
+{
+    // Mark the current node as visited and print it
+    visited[v] = true;
+    cout << v << " ";
+
+    // Recur for all the vertices adjacent to this vertex
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFSUtil(*i, visited);
+}
+
+// DFS traversal of the vertices reachable from v. 
+// It uses recursive DFSUtil()
+void Graph::DFS(int v)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    // Call the recursive helper function to print DFS traversal
+    DFSUtil(v, visited);
+}
+
+int main()
+{
+    // Create a graph given in the above diagram
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(2, 0);
+    g.addEdge(2, 3);
+    g.addEdge(3, 3);
+
+    cout << "Following is Depth First Traversal (starting from vertex 2) \n";
+    g.DFS(2);
+
+    return 0;
+}
