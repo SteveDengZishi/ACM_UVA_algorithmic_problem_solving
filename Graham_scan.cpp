480d91c9c8fd288a2a095b49c2564fad108e1cd4
SteveDengZishi <zd475@nyu.edu>
2017-04-29 16:00:41 -0400

    graham_scan

---
+++ Graham_scan.cpp
@@ -0,0 +1,8 @@
+//  Graham Scan Algorithm for Convex Hull
+//
+//  Created by Steve DengZishi on 4/29/17.
+//  Copyright © 2017 Steve DengZishi. All rights reserved.
+//
+//  1. Anchor point P0 is the one with lowest y coordinates (leftmost if tied)
+//  2. Sort by polar angles (counter clockwise order)
+//  3. 
